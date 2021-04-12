e3919c549b9d368eec5214ebea18385a908cc872
lee <lee.hudson1384@yahoo.co.uk>
2021-04-12 19:43:46 +0100

    Started moving to class

--- synchro.py
+++ synchro.py
@@ -11,129 +11,136 @@ from datetime import datetime
 import smtplib
 
 
-def logMessage(logFileDir,message):
-    now = datetime.now()
-    fileName = logFileDir+"/log_"+now.strftime("%d_%m_%Y") + ".txt"
-    f = open(fileName, "a")
-    f.write(message + " " + os.uname()[1] + " " + now.strftime("%d/%m/%Y/ %H:%M:%S") + "\n")
-    f.close()
+class backupHelper:
+    def __init__(self,errLogFlile,repoLocation,logFileDir,pollPeriod):
+        self.errLogFlile = errLogFlile
+        self.repoLocation = repoLocation
+        self.logFileDir = logFileDir
+        self.pollPeriod = pollPeriod
+
+    def logMessage(self,message):
+        now = datetime.now()
+        fileName = self.logFileDir+"/log_"+now.strftime("%d_%m_%Y") + ".txt"
+        f = open(fileName, "a")
+        f.write(message + " " + os.uname()[1] + " " + now.strftime("%d/%m/%Y/ %H:%M:%S") + "\n")
+        f.close()
 
 
-def logError(errLogFlile,error):
-    now = datetime.now()
-    f = open(errLogFlile, "a")
-    f.write(error + " " + os.uname()[1] + " " + now.strftime("%d/%m/%Y/ %H:%M:%S") + "\n")
-    f.close()
+    def logError(self,error):
+        now = datetime.now()
+        f = open(self.errLogFlile, "a")
+        f.write(error + " " + os.uname()[1] + " " + now.strftime("%d/%m/%Y/ %H:%M:%S") + "\n")
+        f.close()
 
-def sendEmail(subject, message,errLogFlile,logFileDir):
+    def sendEmail(self,subject, message):
 
-    try:
-        server = smtplib.SMTP("smtp.mail.yahoo.com",587)
-        server.starttls()
+        try:
+            server = smtplib.SMTP("smtp.mail.yahoo.com",587)
+            server.starttls()
 
-        server.login("tim.smith361@yahoo.com",'nysvfvgmjwiktrog')
-        message = 'Subject: {}\n\n{}'.format(subject, message)
+            server.login("tim.smith361@yahoo.com",'nysvfvgmjwiktrog')
+            message = 'Subject: {}\n\n{}'.format(subject, message)
 
-        server.sendmail("tim.smith361@yahoo.com","lee.hudson1384@gmail.com",message)
-        server.quit()
-        logMessage(logFileDir,"Email sent ")
-        return 0
-    except:
-        logError(errLogFlile,"Unable to send email")
-        return 1
-
-
-# Moves logfile and sends email with contents
-def clearLogFile(errLogFlile,logFileDir):
-    now = datetime.now()
-    message = ""
-
-    try:
-        file = open(errLogFlile,"r+")
-        for line in file.readlines():
-            message+=line
-        file.close()
-        if sendEmail(os.uname()[1]+" Logfile contents",message,errLogFlile,logFileDir) == 0:
-            os.system("mv "+errLogFlile+" "+errLogFlile+"_"+now.strftime("%d_%m_%Y__%H_%M_%S"))
-    except:
-        print "No log file \n"
+            server.sendmail("tim.smith361@yahoo.com","lee.hudson1384@gmail.com",message)
+            server.quit()
+            logMessage(self.logFileDir,"Email sent ")
+            return 0
+        except:
+            logError(self.errLogFlile,"Unable to send email")
+            return 1
 
 
-# If merge conflict we have to create a branch with the stuff that conflicts and deal with it later
-def mergeConflict(errLogFlile,logFileDir):
-    now = datetime.now()
-    mergeConflictBranch = now.strftime("%d_%m_%Y__%H_%M_%S")
-    mergeMessage = "Merge conflict " + os.uname()[1]
+    # Moves logfile and sends email with contents
+    def clearLogFile(self):
+        now = datetime.now()
+        message = ""
 
-    result = Popen(['/usr/bin/git', 'checkout', '-b', mergeConflictBranch ],stdout=PIPE,stderr=PIPE,shell=False)
-    result = Popen(['/usr/bin/git', 'add', '*'],stdout=PIPE,stderr=PIPE,shell=False)
-    result = Popen(['/usr/bin/git', 'commit', '-m',mergeMessage],stdout=PIPE,stderr=PIPE,shell=False)
-    result = Popen(['/usr/bin/git', 'push', '-u', 'origin', mergeConflictBranch],stdout=PIPE,stderr=PIPE,shell=False)
-    result = Popen(['/usr/bin/git', 'checkout', 'master'],stdout=PIPE,stderr=PIPE,shell=False)
-    result = Popen(['/usr/bin/git', 'pull', 'origin', 'master'],stdout=PIPE,stderr=PIPE,shell=False)
-
-    # Now send email
-    subject = os.uname()[1]+" Merge confict!!"
-    message = "mergeConflict() called by " + os.uname()[1] + "\n"
-    message += mergeConflictBranch + " created and pushed\n"
-    message += "This means something had changed before a pull was done\n"
-    sendEmail(subject,message,errLogFlile,logFileDir)
-
-# Basic commitlogFileDir
-def commit(location,errLogFlile,logFileDir):
-
-    os.chdir(location)
-
-    mergeMessage = "Auto commit " + os.uname()[1]
-
-    # Git add
-    result =  Popen(['/usr/bin/git', 'add', '*'],stdout=PIPE,stderr=PIPE,shell=False)
-    (out, error) = result.communicate()
-    if len(out) > 1:
-        logMessage(logFileDir,"commit() performing git add *\n" +out)
-    if len(error) > 1:
-        logMessage(logFileDir,"commit() performing git add *\n" +error)
-
-    # Git commit
-    result = Popen(['/usr/bin/git', 'commit', '-m',mergeMessage],stdout=PIPE,stderr=PIPE,shell=False)
-    (out, error) = result.communicate()
-    if len(out) > 1:
-        logMessage(logFileDir,"commit() performing git commit\n" +out)
-    if len(error) > 1:
-        logMessage(logFileDir,"commit() performing git commit\n" +error)
-
-    # Git push
-    result = Popen(['/usr/bin/git', 'push', 'origin', 'master'],stdout=PIPE,stderr=PIPE,shell=False)
-    (out, error) = result.communicate()
-    if len(out) > 1:
-        logMessage(logFileDir,"commit() performing git push origin master \n" +out)
-    if len(error) > 1:
-        logMessage(logFileDir,"commit() performing git push origin master \n" +error)
-    
-    if error.find("Could not resolve hostname") != -1:
-            logError(errLogFlile,"No connection")
+        try:
+            file = open(self.errLogFlile,"r+")
+            for line in file.readlines():
+                message+=line
+            file.close()
+            if sendEmail(os.uname()[1]+" Logfile contents",message,self.errLogFlile,self.logFileDir) == 0:
+                os.system("mv "+self.errLogFlile+" "+self.errLogFlile+"_"+now.strftime("%d_%m_%Y__%H_%M_%S"))
+        except:
+            print "No log file \n"
 
 
+    # If merge conflict we have to create a branch with the stuff that conflicts and deal with it later
+    def mergeConflict(self):
+        now = datetime.now()
+        mergeConflictBranch = now.strftime("%d_%m_%Y__%H_%M_%S")
+        mergeMessage = "Merge conflict " + os.uname()[1]
 
-def smartPull(location,errLogFlile,logFileDir):
-
-    os.chdir(location)
-    try:
+        result = Popen(['/usr/bin/git', 'checkout', '-b', mergeConflictBranch ],stdout=PIPE,stderr=PIPE,shell=False)
+        result = Popen(['/usr/bin/git', 'add', '*'],stdout=PIPE,stderr=PIPE,shell=False)
+        result = Popen(['/usr/bin/git', 'commit', '-m',mergeMessage],stdout=PIPE,stderr=PIPE,shell=False)
+        result = Popen(['/usr/bin/git', 'push', '-u', 'origin', mergeConflictBranch],stdout=PIPE,stderr=PIPE,shell=False)
+        result = Popen(['/usr/bin/git', 'checkout', 'master'],stdout=PIPE,stderr=PIPE,shell=False)
         result = Popen(['/usr/bin/git', 'pull', 'origin', 'master'],stdout=PIPE,stderr=PIPE,shell=False)
+
+        # Now send email
+        subject = os.uname()[1]+" Merge confict!!"
+        message = "mergeConflict() called by " + os.uname()[1] + "\n"
+        message += mergeConflictBranch + " created and pushed\n"
+        message += "This means something had changed before a pull was done\n"
+        sendEmail(subject,message)
+
+    # Basic commitlogFileDir
+    def commit(self):
+
+        os.chdir(self.repoLocation)
+
+        mergeMessage = "Auto commit " + os.uname()[1]
+
+        # Git add
+        result =  Popen(['/usr/bin/git', 'add', '*'],stdout=PIPE,stderr=PIPE,shell=False)
         (out, error) = result.communicate()
         if len(out) > 1:
-            logMessage(logFileDir,"smartPull() performing git pull origin master \n" +out)
+            logMessage("commit() performing git add *\n" +out)
         if len(error) > 1:
-            logMessage(logFileDir,"smartPull() performing git pull origin master \n" +error)
+            logMessage("commit() performing git add *\n" +error)
 
-        if error.find("Could not resolve hostname") != -1:
-            logError(errLogFlile,"No connection")
+        # Git commit
+        result = Popen(['/usr/bin/git', 'commit', '-m',mergeMessage],stdout=PIPE,stderr=PIPE,shell=False)
+        (out, error) = result.communicate()
+        if len(out) > 1:
+            logMessage("commit() performing git commit\n" +out)
+        if len(error) > 1:
+            logMessage("commit() performing git commit\n" +error)
+
+        # Git push
+        result = Popen(['/usr/bin/git', 'push', 'origin', 'master'],stdout=PIPE,stderr=PIPE,shell=False)
+        (out, error) = result.communicate()
+        if len(out) > 1:
+            logMessage("commit() performing git push origin master \n" +out)
+        if len(error) > 1:
+            logMessage("commit() performing git push origin master \n" +error)
         
-        if error.find("overwritten by merge") != -1:
-            mergeConflict(errLogFlile,logFileDir)
+        if error.find("Could not resolve hostname") != -1:
+                logError("No connection")
 
-    except Exception as e:
-        logError(errLogFlile,e)
+
+
+    def smartPull(location,errLogFlile,logFileDir):
+
+        os.chdir(location)
+        try:
+            result = Popen(['/usr/bin/git', 'pull', 'origin', 'master'],stdout=PIPE,stderr=PIPE,shell=False)
+            (out, error) = result.communicate()
+            if len(out) > 1:
+                logMessage(logFileDir,"smartPull() performing git pull origin master \n" +out)
+            if len(error) > 1:
+                logMessage(logFileDir,"smartPull() performing git pull origin master \n" +error)
+
+            if error.find("Could not resolve hostname") != -1:
+                logError(errLogFlile,"No connection")
+            
+            if error.find("overwritten by merge") != -1:
+                mergeConflict(errLogFlile,logFileDir)
+
+        except Exception as e:
+            logError(errLogFlile,e)
 
 
 
